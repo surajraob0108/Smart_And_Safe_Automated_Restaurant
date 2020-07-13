cc016569b485a001b3848bcb0a67ec33a307ab8c
Suraj Rao Bappanadu <surajraob@gmail.com>
2020-07-13 14:07:48 +0000

    aws_publisher_subscriber_raspberry_pi4_final.py edited online with Bitbucket

--- Team_16_Project/Code/RaspberryPI/aws_publisher_subscriber_raspberry_pi4_final.py
+++ Team_16_Project/Code/RaspberryPI/aws_publisher_subscriber_raspberry_pi4_final.py
@@ -10,15 +10,20 @@ from PIL import Image
 from resizeimage import resizeimage
 import base64
 import os
+import sys
 from firebase import firebase
+from datetime import date
 
+sensor_health_status = ["Good","Good","Good","Good","Good","Good","Good"]
 # Camera Input
 def camera_input():
-    return_value = os.system("wget 192.168.0.3:8080/photo.jpg")
+    return_value = os.system("wget 192.168.0.2:8080/photo.jpg >/dev/null 2>&1")
     time.sleep(1)
     if(return_value == 1024):
+        sensor_health_status[1] = "Bad"
         f = open("/home/pi/Public/Team_16_Project/Sensor_Programs/photo.jpg", "r+b")
     else:
+        sensor_health_status[1] = "Good"
         f = open("/home/pi/Public/Team_16_Project/Sensor_Programs/Programs/photo.jpg", "r+b")
     image = Image.open(f)
     cover = resizeimage.resize_cover(image, [200, 100])
@@ -30,14 +35,17 @@ def camera_input():
     image_data = image_64_encode.decode("utf-8")
     if(return_value != 1024):
         os.remove("/home/pi/Public/Team_16_Project/Sensor_Programs/Programs/photo.jpg")
-    os.remove("/home/pi/Public/Team_16_Project/Sensor_Programs/Programs/photo_resize.jpg")    
+    os.remove("/home/pi/Public/Team_16_Project/Sensor_Programs/Programs/photo_resize.jpg")
+    print("Image Captured")
     return image_data
 
 # Customer_ID Generation based on distance 
-def customer_id_generation(input_distance):    
+def customer_id_generation(input_distance):
     input_distance_value = int(input_distance)
+    sensor_health_status[0] = "Good"
     if(input_distance_value == 0):
-        customer_id = 0000000000
+        customer_id = 2345678900
+        sensor_health_status[0] = "Bad"
     elif(input_distance_value >=1 and input_distance_value <= 4):
         customer_id = 1234567888
     elif(input_distance_value >= 5 and input_distance_value <= 8):
@@ -64,12 +72,15 @@ def customer_id_generation(input_distance):
     return customer_id
 
 # Temperature Input
-def temperature_sensor_input(pin):
+def temperature_sensor_input(pin):    
+    sensor_health_status[2] = "Good"
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin,2,1)
     # Default value in case if the temperature sensor not working
     if(str(temperature) == "None"):
-        temperature= 20
-    print(temperature)
+        temperature= 50
+        sensor_health_status[2] = "Bad"
+        
+    print("Temperature:"+str(temperature))
     return temperature  
     
 # Distance calculation Input
@@ -89,28 +100,30 @@ def distance_calculation(Trigger_IN,ECHO_OUT,sleeptime):
 
 # Function for Customer Hand Movement Detection
 def customer_movement_hand_detection(PIN_IN,PIN_OUT,sleep_time):
+    sensor_health_status[6] = "Good"
     while True:
-        GPIO.output(PIN_IN, GPIO.LOW)
-        time.sleep(sleep_time)
-        GPIO.output(PIN_IN, GPIO.HIGH)
-        time.sleep(0.00001)
-        GPIO.output(PIN_IN, GPIO.LOW)
-        while GPIO.input(PIN_OUT)==0:
-            pulse_start_time = time.time()
-        while GPIO.input(PIN_OUT)==1:
-            pulse_end_time = time.time()
-        pulse_duration = pulse_end_time - pulse_start_time
-        distance = round(pulse_duration * 17150, 2)        
-        payload_handdetection = {"Restaurant_number":restaurant_number,"Distance":distance}
+        hand_distance = distance_calculation(PIN_IN,PIN_OUT,sleep_time)
+        if(int(hand_distance) == 0):
+            sensor_health_status[6] = "Bad"
+        payload_handdetection = {"Restaurant_number":restaurant_number,"Distance":hand_distance}
         publisher_data(topic_customer_handdetection,payload_handdetection)
         time.sleep(2)
 
 # Customer Detection Table Check
 def customer_presence_table_data(PIN_IN_1,PIN_OUT_1,sleep_time_1,PIN_IN_2,PIN_OUT_2,sleep_time_2,PIN_IN_3,PIN_OUT_3,sleep_time_3):
+    sensor_health_status[3] = "Good"
+    sensor_health_status[4] = "Good"
+    sensor_health_status[5] = "Good"
     while True:
         table_1_distance = distance_calculation(PIN_IN_1,PIN_OUT_1,sleep_time_1)
         table_2_distance = distance_calculation(PIN_IN_2,PIN_OUT_2,sleep_time_2)
         table_3_distance = distance_calculation(PIN_IN_3,PIN_OUT_3,sleep_time_3)
+        if(int(table_1_distance) == 0):
+            sensor_health_status[3] = "Bad"            
+        if(int(table_2_distance) == 0):
+            sensor_health_status[4] = "Bad"
+        if(int(table_3_distance) == 0):
+            sensor_health_status[5] = "Bad"        
         payload_table_data = {"Location":restaurant_location,"Restaurant_number":restaurant_number,"Distance_Table1":table_1_distance,"Distance_Table2":table_2_distance,"Distance_Table3":table_3_distance}
         publisher_data(topic_customer_detection,payload_table_data)
         time.sleep(10)
@@ -120,10 +133,10 @@ def customer_entry_details(input_trig_pin,input_echo_pin,sleeptime,temperature_p
         distance_id = distance_calculation(input_trig_pin,input_echo_pin,sleeptime)
         if(distance_id <=40):
             customer_unique_id = customer_id_generation(distance_id)
-            print(customer_unique_id)
+            print("CustomerID:"+str(customer_unique_id))
             time.sleep(2)
             face_image_data = camera_input()
-            time.sleep(2)
+            time.sleep(1)
             temperature_data = temperature_sensor_input(temperature_pin)
             payload_customer_data = {"Location":restaurant_location,"Restaurant_number":restaurant_number,"Customer_ID":customer_unique_id,"Face_Image":face_image_data,"Body_Temperature":temperature_data}
             publisher_data(topic_customer_details,payload_customer_data)
@@ -159,21 +172,34 @@ def disinfectant_status(input_table_number,input_led_state):
     if(int(input_table_number) == 1):
         if(int(input_led_state) == 1):
             led_on_off(LED_TABLE1_OUT,1)
-        else:
-            print("Table "+str(input_table_number)+":Disinfectant OFF")
     elif(int(input_table_number) == 2):
         if(int(input_led_state) == 1):
             led_on_off(LED_TABLE2_OUT,1)
-        else:
-            print("Table "+str(input_table_number)+":Disinfectant OFF")
     elif(int(input_table_number) == 3):
         if(int(input_led_state) == 1):
             led_on_off(LED_TABLE3_OUT,1)
-        else:
-            print("Table "+str(input_table_number)+":Disinfectant OFF")
     else:
         print("Not a valid table value")
-        
+
+# Writing Sensor Health Status to Database
+def writeToFirebase():
+    while True:
+        if "Bad" in sensor_health_status:
+            print("DataBase entered")
+            firebase1 = firebase.FirebaseApplication('https://sssr-sensorhealth.firebaseio.com/', None)
+
+            data1 = {'Restaurant_ID': restaurant_number,
+                     'ID_Ultrasonic_Distance_Sensor': sensor_health_status[0],
+                     'Camera': sensor_health_status[1],
+                     'Temperature_Sensor': sensor_health_status[2],                     
+                     'Table1_Ultrasonic_Distance_Sensor': sensor_health_status[3],
+                     'Table2_Ultrasonic_Distance_Sensor': sensor_health_status[4],
+                     'Table3_Ultrasonic_Distance_Sensor': sensor_health_status[5],
+                     'Hand_Detection_Ultrasonic_Distance_Sensor': sensor_health_status[6]
+                     }
+            firebase1.post("location/" + restaurant_location + "/" + date.today().strftime('%d-%m-%Y'), data1)
+            print("Sensor Health issue.Run Diagnostics..!!")
+        time.sleep(30)
 # Function to call publisher to send data
 def publisher_data(input_topic_name,payload_data):
     publish_data = json.dumps(payload_data,indent=4)
@@ -189,10 +215,11 @@ def subscriber_start(input_subscriber_topic_name):
 # Confirmation on connection 
 def on_connect(client, userdata, flags, rc):
     print ("Connection status: {}".format(rc))
-    
+
+# Confirmation on message callback    
 def on_message(client, userdata, msg):
     if(msg.topic == topic_customer_details):
-        payload_customer_result = json.loads(msg.payload)
+        payloadblockPrint_customer_result = json.loads(msg.payload)
         customer_entry_result(payload_customer_result["Customer_Entry_Result"])
     elif(msg.topic == topic_customer_detection):
         payload_data = json.loads(msg.payload)
@@ -222,6 +249,9 @@ def valid_restaurant_number():
         int_restaurant_number = int(restaurant_number)
         if(int_restaurant_number == 0 or int_restaurant_number ==1 or int_restaurant_number ==2 or int_restaurant_number ==3):
             print("Entered Restaurant number is correct")
+            print("Customer Scanning Started..!!")
+            print("Hand Santizer:Processing Hand detection Started..!!")
+            print("Table Occupancy Check Started..!!")
             break;
         else:
             print("Please enter a valid restaurant number")
@@ -279,12 +309,14 @@ try:
     t1.start()
     t2 = threading.Thread(target=customer_presence_table_data,args=[PIN_TRIGGER_IN[1],PIN_ECHO_OUT[1],0.15,PIN_TRIGGER_IN[2],PIN_ECHO_OUT[2],0.2,PIN_TRIGGER_IN[3],PIN_ECHO_OUT[3],0.3],daemon=True)
     t2.start()
-    t5 = threading.Thread(target=customer_movement_hand_detection,args=[PIN_TRIGGER_IN[4],PIN_ECHO_OUT[4],0.4],daemon=True)
+    t3 = threading.Thread(target=customer_movement_hand_detection,args=[PIN_TRIGGER_IN[4],PIN_ECHO_OUT[4],0.4],daemon=True)
+    t3.start()
+    t4 = threading.Thread(target=subscriber_start,args=[topic_customer_detection],daemon=True)
+    t4.start()
+    t5 = threading.Thread(target=subscriber_start,args=[topic_customer_handdetection],daemon=True)
     t5.start()
-    t6 = threading.Thread(target=subscriber_start,args=[topic_customer_detection],daemon=True)
+    t6 = threading.Thread(target=writeToFirebase,args=[],daemon=True)
     t6.start()
-    t7 = threading.Thread(target=subscriber_start,args=[topic_customer_handdetection],daemon=True)
-    t7.start()
     
     t0.join()
     t1.join()
@@ -293,7 +325,6 @@ try:
     t4.join()
     t5.join()
     t6.join()
-    t7.join() 
     
 except KeyboardInterrupt:
     GPIO.cleanup()
